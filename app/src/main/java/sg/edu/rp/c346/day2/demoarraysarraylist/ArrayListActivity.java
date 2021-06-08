414d2a1361209ecd84843aefd48c1e01ec31f5b1
19015254 <19015254@myrp.edu.sg>
2021-06-08 10:25:59 +0800

    Completed Activity

--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ArrayListActivity extends AppCompatActivity {
 
     TextView tv;
+    ArrayList<String> fruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,29 @@ public class ArrayListActivity extends AppCompatActivity {
 
         //continue view the code in the worksheet
 
+        fruits = new ArrayList<String>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("cherry");
 
+        //Question 2a
+        String text = "Fruits\n====\n";
+        for (int i = 0; i < fruits.size(); i++)
+        {
+            text += fruits.get(i) + "\n";
+        }
+        tv.setText(text);
+
+        //Question 2b
+        fruits.add("durian");
+
+        //Question 2c
+        String theFruit = fruits.get(1);
+
+        //Question 2d
+        fruits.remove(0);
+
+        //Question 2e
+        fruits.set(fruits.size()-1, "dragonfruit");
     }
 }
